4f3521b4610a8d43463afb6da0d8747795659979
GustavoKruger <gustavokruger514@gmail.com>
2024-04-25 22:13:52 -0300

    criado por Gustavo Kruger-Primeiro Commit back

---
+++ src/projetogithub/ProjetoGitHub.java
@@ -0,0 +1,16 @@
+package projetogithub;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class ProjetoGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro projeto GitHub");
+    }
+    
+}
